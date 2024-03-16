2f986beca8f582dd2c05ed4a2db54c9a43047e85
demostanis <demostanis@protonmail.com>
2024-03-16 21:36:47 +0100

    remove useless constructors for commands

--- commands/Nick.cpp
+++ commands/Nick.cpp
@@ -6,19 +6,11 @@
 /*   By: cgodard <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 17:09:44 by cgodard           #+#    #+#             */
-/*   Updated: 2024/03/15 18:29:21 by cgodard          ###   ########.fr       */
+/*   Updated: 2024/03/16 21:36:29 by cgodard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "commands/Nick.hpp"
-
-		Nick::Nick()
-{
-}
-
-		Nick::~Nick()
-{
-}
 
 void	Nick::handle(IrcMessage &msg)
 {
